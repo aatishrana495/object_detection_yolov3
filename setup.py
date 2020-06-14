3f5fbc14191eea2b119a30772060cbf071e27e05
aatishrana495 <aatishrana495@gmail.com>
2020-06-15 03:28:33 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='object_detection_yolov3',
+      version='0.1',
+      url='https://github.com/aatishrana495/object_detection_yolov3.git',
+      author='Aatish Rana',
+      author_email='aatishrana495@gmail.com',
+      packages=['yolov3'])
